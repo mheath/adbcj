1d493bf958824eff1b7a5888d2cc5ead6de04c1c
mheath <mheath@0ba5e429-e343-0410-a410-798456a35785>
2007-11-07 06:33:19 +0000

    Simplified hashCode() method.
    git-svn-id: https://adbcj.googlecode.com/svn/adbcj/trunk@91 0ba5e429-e343-0410-a410-798456a35785

--- api/src/main/java/org/safehaus/adbcj/support/DefaultField.java
+++ api/src/main/java/org/safehaus/adbcj/support/DefaultField.java
@@ -168,10 +168,7 @@ public class DefaultField implements Field {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + index;
-		return result;
+		return index;
 	}
 
 	@Override
