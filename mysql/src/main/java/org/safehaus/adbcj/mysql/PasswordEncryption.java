69d2c34036e2d29a02157772c3346128f057f4ac
elcapo <elcapo@0ba5e429-e343-0410-a410-798456a35785>
2008-01-25 17:09:23 +0000

    Since we're no longer using the code that was copy and pasted from the MySQL JDBC driver (which isn't even used by the newer MySQL protocol) I removed the MySQL copyright and GPL header.
    The only code in this file is the reverse engineered SHA-1 based password hashing which was not taken from the MySQL code base and therefor not GPL and not Copyrighted by MySQL AB.
    
    git-svn-id: https://adbcj.googlecode.com/svn/adbcj/trunk@133 0ba5e429-e343-0410-a410-798456a35785

--- mysql/src/main/java/org/safehaus/adbcj/mysql/PasswordEncryption.java
+++ mysql/src/main/java/org/safehaus/adbcj/mysql/PasswordEncryption.java
@@ -1,26 +1,18 @@
 /*
- Copyright (C) 2002-2007 MySQL AB
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of version 2 of the GNU General Public License as 
- published by the Free Software Foundation.
-
- There are special exceptions to the terms and conditions of the GPL 
- as it is applied to this software. View the full text of the 
- exception in file EXCEPTIONS-CONNECTOR-J in the directory of this 
- software distribution.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-
-
+ *   Copyright (c) 2007 Mike Heath.  All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
  */
 package org.safehaus.adbcj.mysql;
 
