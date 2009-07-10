63de0224aac2e308f7fec7245a08d4f975066def
mheath <mheath@apache.org>
2009-07-10 11:05:38 -0600

    Updated test to fetch large results.

--- postgresql/mina/src/test/java/PgTest.java
+++ postgresql/mina/src/test/java/PgTest.java
@@ -21,9 +21,9 @@ public class PgTest {
 		ConnectionManager cm = ConnectionManagerProvider.createConnectionManager("adbcj:postgresql-mina://localhost/adbcjtck", "adbcjtck", "adbcjtck");
 		Connection connection = cm.connect().get();
 	
-		final ResultSet rs = connection.executeQuery("SELECT 1, 2, 3").get();
+		final ResultSet rs = connection.executeQuery("SELECT * FROM large").get();
 		for (Row row : rs) {
-			System.out.println(row.get(0) + " " + row.get(1) + " " + row.get(2));
+			System.out.println(row.get(0) + " " + row.get(1));
 		}
 		cm.close(true).get();
 	}
