04a766b2fd93b1a0f48be92796f8d94af8231878
mheath <mheath@apache.org>
2009-11-27 13:34:12 -0700

    Added -e option to maven so we can see exceptions when they're thrown.

--- perf/runpipeline.sh
+++ perf/runpipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn exec:java -Dexec.mainClass="org.adbcj.perf.PipelineTest" -Dexec.args="$1" -Dexec.classpathScope=test
+mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.PipelineTest" -Dexec.args="$1" -Dexec.classpathScope=test 
