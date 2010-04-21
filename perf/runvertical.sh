ac5a30e61a27a017d88a8e4c0d2a8d5d2c5b3dd0
mheath <mheath@apache.org>
2010-04-20 18:40:26 -0600

    Added parameter to execution script.

--- perf/runvertical.sh
+++ perf/runvertical.sh
@@ -3,8 +3,8 @@
 mvn compile
 
 for i in $(seq 0 10 500); do
-	mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.VerticalBenchmark" -Dexec.args="$i adbcj:mysqlnetty://localhost/adbcjtck" -Dexec.classpathScope=test
-	mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.VerticalBenchmark" -Dexec.args="$i adbcj:jdbc:mysql://localhost/adbcjtck" -Dexec.classpathScope=test
-	mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.VerticalBenchmark" -Dexec.args="$i adbcj:postgresql-netty://localhost/adbcjtck" -Dexec.classpathScope=test
-	mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.VerticalBenchmark" -Dexec.args="$i adbcj:jdbc:postgresql://localhost/adbcjtck" -Dexec.classpathScope=test
+	mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.VerticalBenchmark" -Dexec.args="$i adbcj:mysqlnetty://$1/adbcjtck" -Dexec.classpathScope=test
+	mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.VerticalBenchmark" -Dexec.args="$i adbcj:jdbc:mysql://$1/adbcjtck" -Dexec.classpathScope=test
+	mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.VerticalBenchmark" -Dexec.args="$i adbcj:postgresql-netty://$1/adbcjtck" -Dexec.classpathScope=test
+	mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.VerticalBenchmark" -Dexec.args="$i adbcj:jdbc:postgresql://$1/adbcjtck" -Dexec.classpathScope=test
 done
