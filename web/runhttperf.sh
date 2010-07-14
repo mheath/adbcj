28edd5a9dde91cc39feaa0c480ce51be11ccd6f5
Mike Heath <mheath@tourniquet.(none)>
2010-07-14 13:16:17 -0600

    Added final pool update to 'flush' the logs and indicate that we're done running tests to purge the logs of extra data.

--- web/runhttperf.sh
+++ web/runhttperf.sh
@@ -6,4 +6,4 @@ for i in '1 50 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 9
 	httperf --server $1 --port 8080 --uri $2 --num-calls 100 --num-cons $i >> $3.log
 	echo "-----" >> $3.log
 done
-curl http://$1:8080/poolsize?pool_size=1000
+curl http://$1:8080/poolsize?pool_size=1001
