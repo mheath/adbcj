49a027eec9dc4d46d7550f44b9e629811f9be72f
Mike Heath <elcapo@gmail.com>
2012-02-07 20:20:07 -0700

    Removed the perf module as well. Removed these modules from the pom.

--- perf/runminavsnetty.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mvn exec:java -Dexec.mainClass="org.adbcj.perf.MinaVsNettyBenchmark" -Dexec.args="$1" -Dexec.classpathScope=test
--- perf/runpipeline.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mvn compile
-mvn -e exec:java -Dexec.mainClass="org.adbcj.perf.PipelineTest" -Dexec.args="$1" -Dexec.classpathScope=test 
